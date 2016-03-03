7d2b261976a19f0d775ff1577d935f4a609a5f4f
mayur <mayurpandeuk@gmail.com>
2016-03-03 01:03:26 +0000

    Using _GET variable I have changed the contact file to redirect back to the contact.php file instead of the the external contact-thanks.php file. I will now delete the contact-process.php file and contact-thanks.php file as they are no longer needed

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" ){
 
 	//TODO: Send Email
 
-	header("Location: contact-thanks.php");
+	header("Location: contact.php?status=thanks");
 	exit;
 }
 ?>
@@ -28,42 +28,46 @@ include ('inc/header.php');
 	   <div class ="wrapper">		
 		<h1>Contact</h1>
 	   </div>
-	   
-           <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
+
+	  <?php if (isset($_GET["status"]) AND $_GET["status"] == "thanks") { ?>
+          	<p>Thanks for the email! I&rsquo;ll be in touch shortly.</p>
+	  <?php } else { ?>
+ 
+		   <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
 	
-	   <form method="post" action="contact.php">
+		   <form method="post" action="contact.php">
 		
-		<table>
-			<tr>
-			  <th>
-			    <label for="name">Name: </label>
-			  </th>
-			  <td>
-		            <input type="text" name="name" id="name">
-			  </td>
-			</tr>
-			<tr>
-			  <th>
-			    <label for="email">Email: </label>
-			  </th>
-			  <td>
-		            <input type="text" name="email" id="email">
-			  </td>
-			</tr>
-			<tr>
-			  <th>
-			    <label for="message">Message: </label>
-			  </th>
-			  <td>
-		            <textarea name="message" id="message">Please type message here...</textarea>
-			  </td>
-			</tr>
+			<table>
+				<tr>
+				  <th>
+				    <label for="name">Name: </label>
+				  </th>
+				  <td>
+				    <input type="text" name="name" id="name">
+				  </td>
+				</tr>
+				<tr>
+				  <th>
+				    <label for="email">Email: </label>
+				  </th>
+				  <td>
+				    <input type="text" name="email" id="email">
+				  </td>
+				</tr>
+				<tr>
+				  <th>
+				    <label for="message">Message: </label>
+				  </th>
+				  <td>
+				    <textarea name="message" id="message">Please type message here...</textarea>
+				  </td>
+				</tr>
 		
-		</table>
-		<input type="submit" value="SEND" />
-	   
-	   </form>
-	
+			</table>
+			<input type="submit" value="SEND" />
+		   
+		   </form>
+	 <?php } ?>
 
 	</div>
 
