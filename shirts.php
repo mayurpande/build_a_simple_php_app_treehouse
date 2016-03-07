e4c60e75f00f941e6ddd166f336288417372da35
mayur <mayurpandeuk@gmail.com>
2016-03-07 16:27:15 +0000

    Created for loop to list shirt products array

--- shirts.php
+++ shirts.php
@@ -1,14 +1,39 @@
+<?php
+
+$products = array(
+	"Logo Shirt, Red", 
+	"Mike the Frog Shirt, Black", 
+	"Mike the Frog Shirt, Blue"
+);
+
+?>
+
 <?php 
+
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
 include('inc/header.php'); 
+
 ?>
 
 
 	<div class="section page">
-		<h1>Mike&rsquo;s Full Catalog Of Shirts</h1>
-	
+		
+		<div class="wrapper">
+			
+			<h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+				
+			<ul>
+				<?php foreach ($products as $product){ ?>
+					<li><?php echo $product; ?></li>
+				<?php } ?>
+				
+			</ul>
+				
+		</div>
+		
 	</div>
+
 
 <?php include('inc/footer.php'); ?>
 
