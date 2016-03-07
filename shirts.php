093d6f6ff79ede8a716ca9e0486225da57b9d8a8
mayur <mayurpandeuk@gmail.com>
2016-03-07 18:45:38 +0000

    Added another piece of data to the products array, and displayed the title for each shirt on the listing page

--- shirts.php
+++ shirts.php
@@ -4,42 +4,50 @@ $products = array();
 $products[101] = array(
     "name" => "Logo Shirt, Red",
     "img" => "img/shirts/shirt-101.jpg",
-    "price" => 18   
+    "price" => 18,
+    "product_decription" => "Screen Printed, Fine Quality"   
 );
 $products[102] = array(
     "name" => "Mike the Frog Shirt, Black",
     "img" => "img/shirts/shirt-102.jpg",
-    "price" => 20
+    "price" => 20,
+    "product_decription" => "Screen Printed, Fine Quality"
 );
 $products[103] = array(
     "name" => "Mike the Frog Shirt, Blue",
     "img" => "img/shirts/shirt-103.jpg",    
-    "price" => 20
+    "price" => 20,
+    "product_decription" => "Screen Printed, Fine Quality" 
 );
 $products[104] = array(
     "name" => "Logo Shirt, Green",
     "img" => "img/shirts/shirt-104.jpg",    
-    "price" => 18
+    "price" => 18,
+    "product_decription" => "Screen Printed, Fine Quality" 
 );
 $products[105] = array(
     "name" => "Mike the Frog Shirt, Yellow",
     "img" => "img/shirts/shirt-105.jpg",    
-    "price" => 25
+    "price" => 25,
+    "product_decription" => "Screen Printed, Fine Quality" 
 );
 $products[106] = array(
     "name" => "Logo Shirt, Gray",
     "img" => "img/shirts/shirt-106.jpg",    
-    "price" => 20
+    "price" => 20,
+    "product_decription" => "Screen Printed, Fine Quality" 
 );
 $products[107] = array(
     "name" => "Logo Shirt, Turquoise",
     "img" => "img/shirts/shirt-107.jpg",    
-    "price" => 20
+    "price" => 20,
+    "product_decription" => "Screen Printed, Fine Quality" 
 );
 $products[108] = array(
     "name" => "Logo Shirt, Orange",
     "img" => "img/shirts/shirt-108.jpg",    
     "price" => 25,
+    "product_decription" => "Screen Printed, Fine Quality" 
 );
 
 
@@ -66,6 +74,7 @@ include('inc/header.php');
 					echo "<li>";
 					echo '<a href="#">';
 					echo '<img src="' . $product["img"] . '" alt= "' . $product["name"] . '">';
+					echo $product["name"];
 					echo "<p>View Details</p>";				
 					echo "</a>";
 					} 
