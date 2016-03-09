cefac33c0bb9804d302d934cd396a47077b1ee80
mayur <mayurpandeuk@gmail.com>
2016-03-09 17:24:52 +0000

    Changed list shirt code to call function from products.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include('inc/header.php'); ?>
 
 				<img class="hero" src="img/mike-the-frog.png" alt="Mike the Frog says:">
 				<div class="button">
-					<a href="#">
+					<a href="shitrs.php">
 						<h2>Hey, I&rsquo;m Mike!</h2>
 						<p>Check Out My Shirts</p>
 					</a>
@@ -23,30 +23,16 @@ include('inc/header.php'); ?>
 			<div class="wrapper">
 
 				<h2>Mike&rsquo;s Latest Shirts</h2>
+				
+				<?php include("inc/products.php"); ?>
 
 				<ul class="products">
-					<li><a href="#">
-							<img src="img/shirts/shirt-108.jpg">
-							<p>View Details</p>
-						</a>
-					</li><li>
-						<a href="#">
-							<img src="img/shirts/shirt-107.jpg">
-							<p>View Details</p>
-						</a>
-					</li><li>
-						<a href="#">
-							<img src="img/shirts/shirt-106.jpg">
-							<p>View Details</p>
-						</a>
-					</li><li>
-						<a href="#">
-							<img src="img/shirts/shirt-105.jpg">
-							<p>View Details</p>
-						</a>
-					</li>								
+					<?php	foreach($products as $product_id => $product) {
+									echo get_list_view_html($product_id,$product);			
+								} 
+					?>						
 				</ul>
-
+				
 			</div>
 
 		</div>
