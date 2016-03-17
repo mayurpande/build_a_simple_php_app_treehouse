ab32fb35dc49e24d8c50d2ae05ad1514366bdf39
mayur <mayurpandeuk@gmail.com>
2016-03-17 15:11:49 +0000

    Modified cart link to link to paypal checkout, made use of paypal api

--- inc/header.php
+++ inc/header.php
@@ -17,7 +17,7 @@
 				<li class="shirts <?php if ($section == "shirts") { echo "on";} ?>"><a href="/build_a_simple_php_app_treehouse/shirts.php">Shirts</a></li>
 				<li class="contact <?php if ($section == "contact") {echo "on";} ?>"><a href="/build_a_simple_php_app_treehouse/contact.php">Contact</a></li>
 				<li class="company_info <?php if($section == "company") {echo "on";} ?>"><a href="/build_a_simple_php_app_treehouse/company_info.php">Company Information</a></li>
-				<li class="cart"<a href="#">Shopping Cart</a></li>
+				<li class="cart"><a target="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_cart&amp;business=Q6NFNPFRBWR8S&amp;display=1">Shopping Cart</a></li>
 			</ul>
 
 		</div>
