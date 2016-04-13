e77170275549ba7349d90b2fcb7ae47aae888da1
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-13 14:26:27 +0100

    changes made to urls, now using root-realtive paths

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,10 @@
+
 <html>
 <head>
 	<title><?php echo $pageTitle; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="/css/style.css" type="text/css">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
-	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="shortcut icon" href="/favicon.ico">
 </head>
 <body>
 
@@ -11,11 +12,11 @@
 
 		<div class="wrapper">
 
-			<h1 class="branding-title"><a href="./">Shirts 4 Mike</a></h1>
+			<h1 class="branding-title"><a href="/">Shirts 4 Mike</a></h1>
 
 			<ul class="nav">
-				<li class="shirts <?php if ($section == "shirts") { echo "on";} ?>"><a href="/build_a_simple_php_app_treehouse/shirts.php">Shirts</a></li>
-				<li class="contact <?php if ($section == "contact") {echo "on";} ?>"><a href="/build_a_simple_php_app_treehouse/contact.php">Contact</a></li>
+				<li class="shirts <?php if ($section == "shirts") { echo "on";} ?>"><a href="/shirts/">Shirts</a></li>
+				<li class="contact <?php if ($section == "contact") {echo "on";} ?>"><a href="/contact/">Contact</a></li>
 				<li class="cart"><a target="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_cart&amp;business=Q6NFNPFRBWR8S&amp;display=1">Shopping Cart</a></li>
 			</ul>
 
