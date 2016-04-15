ed36a3b80795562f6b632325cbca0036dcddcc14
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-15 13:39:52 +0100

    added view code for search and controller code for search functionallity

--- search/index.php
+++ search/index.php
@@ -28,25 +28,34 @@ include('../inc/header.php');
             <h1>Search</h1>
             
             <form method="get" action="./">
-                <input type="text" name="s" />
+            <input type="text" name="s" value="<?php echo htmlspecialchars($search_term); ?>" />
                 <input type="submit" value="Go" />
 
             </form>
 
             <?php
                 //if search term isnt blank then a search is being performed
+                
                 if($search_term != ""){
-                    
-                    echo '<ul class="products">';
-                    //we need one list item tag for each shirt
-                    //so we will need a foreach loop
-                    foreach($products as $product){
-                        //in this loop we are considering each loop one by one
-                        //foreach shirt we need to display the list item for that shirt
-                       echo  get_list_view_html($product);
+                    //check to see if search query returns nothing (blank array)
+                    //if the array is not empty as in result are found then display the results
+                    if(!empty($products)){                    
+                        echo '<ul class="products">';
+                        //we need one list item tag for each shirt
+                        //so we will need a foreach loop
+                        foreach($products as $product){
+                            //in this loop we are considering each loop one by one
+                            //foreach shirt we need to display the list item for that shirt
+                           echo  get_list_view_html($product);
 
+                        }
+                        echo'</ul>';
+                    }else{
+                        //if the array is empty and there are not results display this
+                        echo '<p>No products were found mathching that search term.</p>';
                     }
-                    echo'</ul>';
+                   
+                    
                  }
             ?>
         
