5082c9308056814b918c3395b7cd6acb47480d98
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-17 18:59:41 +0100

    Added controller for amount of shirts listed on each page

--- shirts/shirts.php
+++ shirts/shirts.php
@@ -1,11 +1,51 @@
 <?php 
 include('../inc/products.php');
+if(empty($_GET["pg"])){
+    $current_page = 1;
+}else{
+    $current_page = $_GET["pg"];
+}
+$current_page = intval($current_page);
+
+$total_products = get_products_count();
+$products_per_page = 8;
+$total_pages = ceil($total_products / $products_per_page);
+
+if($current_page > $total_pages){
+    header("Location: ./?pg=" . $total_pages);
+}
+
+if($current_page < 1){
+    header("Location: ./");
+}
+
+$start = (($current_page - 1) * $products_per_page) + 1;
+$end = $current_page * $products_per_page;
+if($end > $total_products) {
+    $end = $total_products;
+}
+
+
+
+echo "<pre>";
+echo "Total Products: ";
+echo $total_products;
+echo "\nTotal Pages: ";
+echo $total_pages;
+echo "\nCurrent Page: ";
+echo $current_page;
+echo " (" . $start . "-" . $end . ")";
+exit;
+
+//call new products function, and load return variable with the same name as before
+$products = get_products_subset($start,$end);
+
+
+
+
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
 include('../inc/header.php'); 
-//call new products function, and load return variable with the same name as before
-$products = get_products_all();
-
 ?>
 
 
